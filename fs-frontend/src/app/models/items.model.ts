a7b9bd364267c9474eb6d473d6449b0c5c9aa001
Aman Singh <amansjot@gmail.com>
2024-11-18 22:40:27 -0500

    db uses dates not age

--- fs-frontend/src/app/models/items.model.ts
+++ fs-frontend/src/app/models/items.model.ts
@@ -16,7 +16,7 @@ export interface InventoryItemModel{
 	description: string;
 	animal: string; // dog, cat, bird, fish, etc.
 	breed: string; // breed or species information
-	age: number; // age in years
+	birthdate: Date | null; // age in years
 	quantity: number;
 	price: number;
 	benefits: string[]; // Array of benefits
@@ -32,7 +32,7 @@ export const emptyItem: InventoryItemModel = {
 	description: '',
 	animal: '', // dog, cat, bird, fish, etc.
 	breed: '', // breed or species information
-	age: 0, // age in years
+	birthdate: null, // age in years
 	quantity: 0,
 	price: 0,
 	benefits: [''], // Array of benefits
--- fs-frontend/src/app/pages/home/home.component.ts
+++ fs-frontend/src/app/pages/home/home.component.ts
@@ -120,6 +120,20 @@ export class HomeComponent {
     }
   }
 
+  getAge(birthdate: Date | null): number {
+    if (!birthdate) {
+      return -1;
+    }
+
+    const now = new Date();
+    const birth = new Date(birthdate);
+
+    const diffInMilliseconds = now.getTime() - birth.getTime();
+    const ageInYears = diffInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
+
+    return ageInYears;
+  }
+
   formatAge(ageInYears: number): string {
     if (ageInYears < 1) {
       // Convert to months if less than 1 year
