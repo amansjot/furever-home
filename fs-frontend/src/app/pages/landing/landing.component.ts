995441033172a10e3a08ee94fad79e989de723a6
MaliceKy <kyle.malice2001@gmail.com>
2024-11-21 22:52:39 -0500

    landing page styles renamed & reorganized

--- fs-frontend/src/app/pages/landing/landing.component.ts
+++ fs-frontend/src/app/pages/landing/landing.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.scss'
+  styleUrl: './landing.styles.scss'
 })
 export class LandingComponent {
 
