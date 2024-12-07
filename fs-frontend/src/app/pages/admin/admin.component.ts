de6dd6d7f07d75150b8ebc5c6e90084fa69a8922
Suhas Bolledula <suhasb@udel.edu>
2024-12-07 17:14:14 -0500

    Updates

--- fs-frontend/src/app/pages/admin/admin.component.ts
+++ fs-frontend/src/app/pages/admin/admin.component.ts
@@ -32,9 +32,9 @@ import { MatSort, MatSortModule } from '@angular/material/sort';
   styleUrl: './admin.component.scss',
 })
 export class AdminComponent implements OnInit {
-  @ViewChild(MatSort) sort!: MatSort;
   public loading: boolean = true;
 
+  // Column definitions for pets table
   public petsColumns: string[] = [
     'name',
     'status',
@@ -45,82 +45,86 @@ export class AdminComponent implements OnInit {
     'location',
     'actions',
   ];
-  public pets = new MatTableDataSource<InventoryItemModel>([]);
+  public pets = new MatTableDataSource<InventoryItemModel>([]); // Data source for pets
 
+  // Column definitions for users table
   public userColumns: string[] = [
     'name',
     'location',
     'username',
     'roles'
   ];
-  public users = new MatTableDataSource<UserModel>([]);
+  public users = new MatTableDataSource<UserModel>([]); // Data source for users
 
   constructor(
-    private _loginSvc: LoginService,
-    private itemSvc: ItemService,
-    private profileSvc: ProfileService,
-    private router: Router,
-    private dialog: MatDialog
+    private _loginSvc: LoginService, // Service for authentication and user login
+    private itemSvc: ItemService, // Service for managing items (pets)
+    private profileSvc: ProfileService, // Service for user profiles
+    private router: Router, // Router for navigation
+    private dialog: MatDialog // Service for managing dialogs
   ) {}
 
   ngOnInit(): void {
-    this.loadPets();
-    this.loadUsers();
+    this.loadPets(); // Load pets data on component initialization
+    this.loadUsers(); // Load users data on component initialization
 
+    // Custom filter for pets table
     this.pets.filterPredicate = (data: InventoryItemModel, filter: string) => {
-      const { pictures, ...filteredData } = data;
+      const { pictures, ...filteredData } = data; // Exclude pictures from filtering
       const dataStr = JSON.stringify(filteredData).toLowerCase();
-      return dataStr.includes(filter);
+      return dataStr.includes(filter); // Check if data includes filter value
     };
 
+    // Custom filter for users table
     this.users.filterPredicate = (data: UserModel, filter: string) => {
-      const { password, ...filteredData } = data;
+      const { password, ...filteredData } = data; // Exclude passwords from filtering
       const dataStr = JSON.stringify(filteredData).toLowerCase();
-      return dataStr.includes(filter);
+      return dataStr.includes(filter); // Check if data includes filter value
     };
   }
   
-  ngAfterViewInit() {
-    this.pets.sort = this.sort; // Connect the MatSort to the data source
-  }
-
   async loadUsers(): Promise<void> {
     try {
-      this.users.data = await this.profileSvc.getAllUsers();
+      this.users.data = await this.profileSvc.getAllUsers(); // Fetch users from service
       this.users.data.forEach(user => {
         if (user.roles) {
+          // Sort roles alphabetically
           user.roles = user.roles.sort((a: string, b: string) => a.localeCompare(b));
         }
       });
-      this.loading = false;
+      this.loading = false; // Set loading to false after data is loaded
     } catch (err) {
-      console.error('Error in loadData:', err);
+      console.error('Error in loadData:', err); // Log any errors
       this.loading = false;
       throw err;
     }
   }
 
+  // Load pets from the server
   async loadPets(): Promise<void> {
     try {
-      this.pets.data = await this.itemSvc.getInventoryItems();
-      this.loading = false;
+      this.pets.data = await this.itemSvc.getInventoryItems(); // Fetch pets from service
+      this.loading = false; // Set loading to false after data is loaded
     } catch (err) {
-      console.error('Error in loadData:', err);
+      console.error('Error in loadData:', err); // Log any errors
       this.loading = false;
       throw err;
     }
   }
 
+  // Apply filter to pets table
   applyFilterPets(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.pets.filter = filterValue.trim().toLowerCase();
+    this.pets.filter = filterValue.trim().toLowerCase(); // Set the filter value
   }
   
+  // Apply filter to users table
   applyFilterUsers(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.users.filter = filterValue.trim().toLowerCase();
+    this.users.filter = filterValue.trim().toLowerCase(); // Set the filter value
   }
 
+  // Confirm deletion of a pet
   confirmDelete(itemId: string): void {
     // Open the confirmation dialog
     const dialogRef = this.dialog.open(ConfirmDialogComponent, {
@@ -135,21 +139,22 @@ export class AdminComponent implements OnInit {
     // Handle the user's confirmation
     dialogRef.afterClosed().subscribe((isConfirmed) => {
       if (isConfirmed) {
-        this.deletePet(itemId);
+        this.deletePet(itemId); // Delete the pet if confirmed
       }
     });
   }
 
+  // Delete a pet by ID
   deletePet(itemId: string): void {
     this.itemSvc.deleteItemById(itemId).subscribe({
       next: () => {
         // Update the data of the MatTableDataSource
-        const updatedPets = this.pets.data.filter((pet) => pet._id !== itemId);
+        const updatedPets = this.pets.data.filter((pet) => pet._id !== itemId); // Remove deleted pet
         this.pets.data = updatedPets; // Update the table data
-        console.log('Item deleted successfully');
+        console.log('Item deleted successfully'); // Log success message
       },
       error: (err: any) => {
-        console.error('Error deleting item:', err);
+        console.error('Error deleting item:', err); // Log error message
       },
     });
   }  
